fe0bffdec091306dc863a692e41160fedb435ddd
Brock Atwood <brock.atwood@du.edu>
2021-08-25 18:27:24 -0600

    Engineer test passes

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -2,75 +2,74 @@ const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
   describe("Initialization", () => {
-    it("should create an object with name, id, email, and office number", () => {
-      const Employ = new Manager("Brock", 1, "b@yahoo.com", 2);
+    it("should create an object with name, id, email, and GitHub", () => {
+      const Employ = new Engineer("Brock", 1, "b@yahoo.com", "BrockAtwood");
       expect(Employ.name).toEqual("Brock");
       expect(Employ.id).toEqual(1);
       expect(Employ.email).toEqual("b@yahoo.com");
-      expect(Employ.officeNumber).toEqual(2);
+      expect(Employ.GitHub).toEqual("BrockAtwood");
     });
     it("should cause an error if no argument is provided", () => {
-      const Employ = () => new Manager();
+      const Employ = () => new Engineer();
       expect(Employ).toThrow;
     });
     it("should cause an error if the name is not a string", () => {
-      const Employ = () => new Manager(23, 1, "b@yahoo.com", 2);
+      const Employ = () => new Engineer(23, 1, "b@yahoo.com", "BrockAtwood");
       expect(Employ).toThrow;
     });
     it("should cause an error if the id is not a number", () => {
-      const Employ = () => new Manager("brock", "brock", "b@yahoo.com", 2);
+      const Employ = () =>
+        new Engineer("brock", "brock", "b@yahoo.com", "BrockAtwood");
       expect(Employ).toThrow;
     });
     it("should cause an error if the id is not a positive number", () => {
-      const Employ = () => new Manager("brock", -1, "b@yahoo.com", 2);
+      const Employ = () =>
+        new Engineer("brock", -1, "b@yahoo.com", "BrockAtwood");
       expect(Employ).toThrow;
     });
     it("should cause an error if the email is not a verified email address", () => {
-      const Employ = () => new Manager("brock", 1, "b@bb@yahoo.com", 2);
+      const Employ = () =>
+        new Engineer("brock", 1, "b@bb@yahoo.com", "BrockAtwood");
       expect(Employ).toThrow;
     });
-    it("should cause an error if the office number is not a number", () => {
-      const Employ = () => new Manager("brock", "brock", "b@yahoo.com", 2);
-      expect(Employ).toThrow;
-    });
-    it("should cause an error if the office number is not a positive number", () => {
-      const Employ = () => new Manager("brock", -1, "b@yahoo.com", -2);
+    it("should cause an error if the GitHub is not a string", () => {
+      const Employ = () => new Engineer("brock", "brock", "b@yahoo.com", 2);
       expect(Employ).toThrow;
     });
   });
   describe("getName()", () => {
     it("should return this.name value", () => {
-      const gotName = new Manager("brock", 1, "b@yahoo.com", 2);
+      const gotName = new Engineer("brock", 1, "b@yahoo.com", "BrockAtwood");
       const testingName = gotName.name;
       expect(testingName).toEqual("brock");
     });
   });
   describe("getId()", () => {
     it("should return this.id value", () => {
-      const gotId = new Manager("brock", 1, "b@yahoo.com", 2);
+      const gotId = new Engineer("brock", 1, "b@yahoo.com", "BrockAtwood");
       const testingId = gotId.id;
       expect(testingId).toEqual(1);
     });
   });
   describe("getEmail()", () => {
     it("should return this.email value", () => {
-      const gotEmail = new Manager("brock", 1, "b@yahoo.com", 2);
+      const gotEmail = new Engineer("brock", 1, "b@yahoo.com", "BrockAtwood");
       const testingEmail = gotEmail.email;
       expect(testingEmail).toEqual("b@yahoo.com");
     });
   });
-  describe("getofficeNumber()", () => {
-    it("should return this.officeNumber value", () => {
-      const gotofficeNumber = new Manager("brock", 1, "b@yahoo.com", 2);
-      const testingofficeNumber = gotofficeNumber.officeNumber;
-      expect(testingofficeNumber).toEqual(2);
+  describe("getGitHub()", () => {
+    it("should return this.GitHub value", () => {
+      const gotGitHub = new Engineer("brock", 1, "b@yahoo.com", "BrockAtwood");
+      const testingGitHub = gotGitHub.GitHub;
+      expect(testingGitHub).toEqual("BrockAtwood");
     });
   });
   describe("getRole()", () => {
     it("should return a string", () => {
-      const gotRole = new Manager("brock", 1, "b@yahoo.com");
+      const gotRole = new Engineer("brock", 1, "b@yahoo.com", "BrockAtwood");
       const testingRole = gotRole.getRole();
-      expect(testingRole).toEqual("Manager");
+      expect(testingRole).toEqual("Engineer");
     });
   });
 });
